45a1a642929059d56bee42ade9cdf92ca6cacd84
wortit988 <106458453+wortit988@users.noreply.github.com>
2023-10-04 00:35:19 +0530

    Building Nested Comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const Comment = ({data}) => {
+    const { name, text, replies } = data;
   return (
-    <div>Comment</div>
+    <div className='flex shadow-sm bg-gray-100 p-2 rounded-lg my-2'>
+        <img
+         className='w-12 h-12'
+         alt='user'
+         src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmoGtyISMVoKuWRqAo9KLfl1g84gWVdOvCYk41aKGhUMDKrryDgM3hAYXRmm-Ad1wUaEE' 
+        />
+        <div className='px-3'>
+            <p className='font-bold'>{name}</p>
+            <p>{text}</p>
+        </div>
+    </div>
   )
 }
 
-export default Comment
+export default Comment;
--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -3,33 +3,96 @@ import Comment from './Comment';
 
 const commentsData = [
     {
-        name : "Test",
-        text : "Lorem ipsum dolor sit amet, consectetur adip",
-        replies : [
+      name: "Wortit Test",
+      text: "Lorem ipsum dolor sit amet, consectetur adip",
+      replies: [],
+    },
+    {
+      name: "Wortit Test",
+      text: "Lorem ipsum dolor sit amet, consectetur adip",
+      replies: [
+        {
+          name: "Wortit Test",
+          text: "Lorem ipsum dolor sit amet, consectetur adip",
+          replies: [],
+        },
+        {
+          name: "Wortit Test",
+          text: "Lorem ipsum dolor sit amet, consectetur adip",
+          replies: [
             {
+              name: "Wortit Test",
+              text: "Lorem ipsum dolor sit amet, consectetur adip",
+              replies: [
+                {
+                  name: "Wortit Test",
+                  text: "Lorem ipsum dolor sit amet, consectetur adip",
+                  replies: [
+                    {
+                      name: "Wortit Test",
+                      text: "Lorem ipsum dolor sit amet, consectetur adip",
+                      replies: [
+                        {
+                          name: "Wortit Test",
+                          text: "Lorem ipsum dolor sit amet, consectetur adip",
+                          replies: [],
+                        },
+                      ],
+                    },
+                    {
+                      name: "Wortit Test",
+                      text: "Lorem ipsum dolor sit amet, consectetur adip",
+                      replies: [],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Wortit Test",
+      text: "Lorem ipsum dolor sit amet, consectetur adip",
+      replies: [],
+    },
+    {
+      name: "Wortit Test",
+      text: "Lorem ipsum dolor sit amet, consectetur adip",
+      replies: [],
+    },
+    {
+      name: "Wortit Test",
+      text: "Lorem ipsum dolor sit amet, consectetur adip",
+      replies: [],
+    },
+    {
+      name: "Wortit Test",
+      text: "Lorem ipsum dolor sit amet, consectetur adip",
+      replies: [],
+    },
+  ];
+  
 
-            }
-        ],
-    },
-    {
-        name : "Test",
-        text : "Lorem ipsum dolor sit amet, consectetur adip"
-    },
-    {
-        name : "Test",
-        text : "Lorem ipsum dolor sit amet, consectetur adip"
-    },
-    {
-        name : "Test",
-        text : "Lorem ipsum dolor sit amet, consectetur adip"
-    },
-]
+  const CommentsList = ({ comments }) => {
+    // Disclaimer: Don't use indexes as keys
+    return comments.map((comment, index) => (
+      <div key={index}>
+        <Comment data={comment} />
+        <div className="pl-5 border border-l-black ml-5">
+          <CommentsList comments={comment.replies} />
+        </div>
+      </div>
+    ));
+  };
+
 
 const CommentsContainer = () => {
   return (
     <div className='m-5 p-2'>
         <h1 className='text-2xl font-bold'>Comments : </h1>
-        <Comment data = {commentsData[0]} />
+        <CommentsList comments={commentsData} />
     </div>
   )
 }
