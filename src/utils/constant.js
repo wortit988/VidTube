4e2bff30c2584164676e93c0c0abc2c90719e15d
wortit988 <106458453+wortit988@users.noreply.github.com>
2024-03-14 23:38:48 +0530

    env file added

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,10 +1,12 @@
-const GOOGLE_API_KEY = "AIzaSyA8wajkqRCkVnBGtcbR1eIU8xg8qMBXmBo";
+const GOOGLE_API_KEY = process.env.REACT_APP_API_KEY;
+
+console.log("key : " + process.env.REACT_APP_API_KEY)
 
 export const LIVE_CHAT_COUNT = 25;
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  GOOGLE_API_KEY + "&videoCategoryId=";
+ GOOGLE_API_KEY + "&videoCategoryId=";
 
 export const GET_VIDEO_CATEGORIES_LIST = "https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=IN&key=" + GOOGLE_API_KEY;
 
